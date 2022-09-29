e0662c56254b5da0f4f8c86f5b242c324c8a16b1
Lewis Mullarkey <c1930597@cardiff.ac.uk>
2022-09-29 17:06:28 +0100

    screener

---
+++ filter.js
@@ -0,0 +1,5 @@
+let test = document.getElementsByClassName("tradingview-widget-container")[1]
+console.log(test)
+let iframe = test.childNodes[1]
+console.log(test.childNodes)
+console.log(iframe.contentWindow)
