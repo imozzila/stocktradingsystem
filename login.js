b065314918071693dce527894b64067def62dd8b
Zen Tan <43140815+z3n-t4n@users.noreply.github.com>
2022-10-02 22:54:17 +0800

    Add files via upload

---
+++ login.js
@@ -0,0 +1,11 @@
+function validate() {
+  var username = document.getElementById("username").value;
+  var password = document.getElementById("password").value;
+  if (username == "admin" && password == "password") {
+    alert("login successful");
+    window.open("./index.html");
+  }
+  else {
+    alert("login failed");
+  }
+}
